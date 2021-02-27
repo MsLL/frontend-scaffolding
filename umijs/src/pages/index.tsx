d5c052d0f2c04524e81aa449274788eadc44c70b
upup-ubuntu <2213311288@qq.com>
2021-02-27 21:27:20 +0800

    1.验证umijs react模块，antd模块  2.fix编辑器jsx语法提示

--- umijs/src/pages/index.tsx
+++ umijs/src/pages/index.tsx
@@ -1,9 +1,13 @@
 import styles from './index.less';
+import React from 'react';
+import {Button} from 'antd';
 
 export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      <Button>button</Button>
+      <Button type="primary">buttonxxx</Button>
     </div>
   );
 }
